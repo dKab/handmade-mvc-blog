3a5e02b2ffafe8f070f362b57f8585c5cec8fea3
dKab <dkabardinov@gmail.com>
2014-03-22 11:50:36 +0600

    upd

--- protected/core/Application.php
+++ protected/core/Application.php
@@ -11,18 +11,35 @@ class Application {
     public static function run() {
         $instance = new self();
         $instance->init();
-        $instance->handleRequest();
+        if (AppHelper::instance()->isProduction()) {
+            try {
+                $instance->handleRequest();
+            } catch (Exception $e) {
+                $message = $e->__toString();
+                // устанавливаем логгирование ошибок
+                ini_set("log_errors", 1);
+                ini_set("error_log", "log/php-error.log");
+                // вручную записываем сообщение в лог
+                error_log($message);
+                //показываем заглушку для пользователей и завершаем выполнение
+                header('HTTP/1.0 500 Internal Server Error');
+                echo "<h1>Oops! It seems an error has occured.</h1>"
+                . " The administrator of the site will get a notification. Sorry!";
+            }
+        } else {
+            $instance->handleRequest();
+        }
     }
 
     public function init() {
         $appHelper = AppHelper::instance();
-        $appHelper->init();     
+        $appHelper->init();
         if ($appHelper->isProduction()) {
             ini_set('display_errors', 'Off');
         } else {
-            ini_set('display_errors',1);
+            ini_set('display_errors', 1);
             error_reporting(-1);
-        }   
+        }
     }
 
     private function handleRequest() {
@@ -33,10 +50,11 @@ class Application {
             $action = $dispatcher->getAction($request);
             $controller->execute($action);
         } catch (NotFoundException $ex) {
-                header('HTTP/1.0 404 Not Found');
-                echo "<h1>404 Not Found</h1>";
-                echo "The page you have requested could not be found.";
-                exit();
+            header('HTTP/1.0 404 Not Found');
+            echo "<h1>404 Not Found</h1>";
+            echo "The page you have requested could not be found.";
+            exit();
         }
     }
+
 }
